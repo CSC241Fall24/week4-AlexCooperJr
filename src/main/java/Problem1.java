a1a8c2f33cdc65301540a9922ff84a3b31736e5c
Alex Cooper <cooperjralex@gmail.com>
2024-09-22 15:17:26 -0400

    first commit, result check

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,9 +1,24 @@
 // Problem1.java
+
+import java.util.List;
+
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
-        return null;
+        if(l1==null){
+            return l2;}
+        ListNode pointer=l1.next;
+        ListNode tail=l1.next;
+        while(pointer.next!=null){
+            tail=pointer.next;
+            pointer=pointer.next;
+        }
+        ListNode l3=l2;
+        while(l3.next!=null){
+            tail.next=l3;
+        }
+        return l1;
     }
 }
