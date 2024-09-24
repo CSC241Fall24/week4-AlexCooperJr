4264f54a9456c01eafedb34b2e7e1ae359b8643c
Alex Cooper <cooperjralex@gmail.com>
2024-09-23 23:12:33 -0400

    problem 1 fixed

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,23 +1,36 @@
 // Problem1.java
 
-import java.util.List;
-
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
+        //for empty first list
         if(l1==null){
             return l2;}
-        ListNode pointer=l1.next;
-        ListNode tail=l1.next;
+        //for empty second list
+        if(l2==null){
+            return l1;
+        }
+        
+        ListNode tail1=l1;
+        ListNode pointer=l1;
+    
+
         while(pointer.next!=null){
-            tail=pointer.next;
+            tail1 = pointer.next;
             pointer=pointer.next;
         }
-        ListNode l3=l2;
-        while(l3.next!=null){
-            tail.next=l3;
+
+        pointer = l2;
+
+        while(pointer.next!=null){
+            int num = pointer.val;
+            tail1.next = new ListNode(num);
+            tail1.next.next=pointer.next;
+            pointer=pointer.next;
+
+
         }
         return l1;
     }
